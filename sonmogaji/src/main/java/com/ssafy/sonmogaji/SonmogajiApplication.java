c94b56d23496965c3dc2b800f435d20ddc19ec9b
younggilPark <ygpark1230@gmail.com>
2022-09-14 15:22:56 +0900

    innit upload

---
+++ sonmogaji/src/main/java/com/ssafy/sonmogaji/SonmogajiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.sonmogaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonmogajiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonmogajiApplication.class, args);
+	}
+
+}
---
+++ sonmogaji/src/test/java/com/ssafy/sonmogaji/SonmogajiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sonmogaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonmogajiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
