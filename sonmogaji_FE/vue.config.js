7f62ff3703bb765645b91b54131e28a82869d605
leedeukgyo <dg1411@naver.com>
2022-10-06 21:19:24 +0900

    merge

--- sonmogaji_FE/vue.config.js
+++ sonmogaji_FE/vue.config.js
@@ -5,12 +5,12 @@ module.exports = defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        // target: 'http://localhost:8080'
-        target: "https://j7a308.p.ssafy.io",
+        target: 'http://localhost:8080'
+        // target: "https://j7a308.p.ssafy.io",
       },
       "/room": {
-        // target: 'http://localhost:8080'
-        target: "https://j7a308.p.ssafy.io",
+        target: 'http://localhost:8080'
+        // target: "https://j7a308.p.ssafy.io",
       },
     },
   },
